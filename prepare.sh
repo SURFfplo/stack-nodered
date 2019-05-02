0af81738227c76c4c5b27f1d8ca21a55243b62e8
Herman <herman@dompseler.nl>
2019-05-02 17:14:29 +0200

    maak een stack bruikbaar voor meerdere instanties op de swarm - verschillende namen, versies, netwerken en poorten

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # create nfs mount
-mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE-$STACK_VERSION/data
+mkdir -p /mnt/nfs/nfsdlo/$STACK_NETWORK/$STACK_SERVICE/$STACK_VERSION/data
