c3519b27ab5298d1ea8af115b5adb29b7123c5ca
Herman <herman@dompseler.nl>
2019-07-01 10:19:20 +0200

    prepare for multiple swarm servers

--- go.sh
+++ go.sh
@@ -34,5 +34,4 @@ docker stack rm $STACK_SERVICE
 ./prepare.sh
 
 # go
-docker stack deploy -c docker-compose.yml $STACK_SERVICE
-
+docker stack deploy --with-registry-auth -c docker-compose.yml $STACK_SERVICE
